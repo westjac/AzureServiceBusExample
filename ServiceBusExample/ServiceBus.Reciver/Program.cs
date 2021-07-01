5584f585e320cc77ebbdf6c2d69a482af7b999a5
Jacob West <jwest@omnitech-inc.com>
2021-07-01 15:04:15 -0500

    Added reciver project and installed some nuget packages to it

---
+++ ServiceBusExample/ServiceBus.Reciver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBus.Reciver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
